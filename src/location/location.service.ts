e1ed96ab493f0d652c865777b5357861e552f780
Diana Kinosian <d.kinosyan@avto.pro>
2024-12-24 11:05:10 +0100

    return types

--- src/location/location.service.ts
+++ src/location/location.service.ts
@@ -2,12 +2,13 @@ import { HttpService } from '@nestjs/axios';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { firstValueFrom } from 'rxjs';
 import { ErrorMessages } from 'src/main/constants/messages.constants';
+import { Country } from './types';
 
 @Injectable()
 export class LocationService {
   constructor(private readonly httpService: HttpService) {}
 
-  async getCountries() {
+  async getCountries(): Promise<Country[]> {
     try {
       const response = await firstValueFrom(
         this.httpService.get(
@@ -24,7 +25,7 @@ export class LocationService {
     }
   }
 
-  async getCitiesByCountry(country: string) {
+  async getCitiesByCountry(country: string): Promise<string[]> {
     try {
       const response = await firstValueFrom(
         this.httpService.post(
@@ -33,7 +34,7 @@ export class LocationService {
         ),
       );
 
-      return response.data;
+      return response.data.data;
     } catch (error) {
       throw new HttpException(
         error.response?.data || ErrorMessages.FAILED_FETCH_CITIES,
---
+++ src/location/types.ts
@@ -0,0 +1,6 @@
+export type Country = {
+  name: string;
+  capital: string;
+  iso2: string;
+  iso3: string;
+};
